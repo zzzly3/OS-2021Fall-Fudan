7f39dca96506ddf61838feea1729c094c46b625e
Tan-YiFan <15711657500@163.com>
2021-09-17 15:26:45 +0800

    lab1

--- src/main.c
+++ src/main.c
@@ -6,4 +6,12 @@ NORETURN void main() {
     init_console();
 	/* TODO: Lab1 print */
     puts("Hello world!");
+    puts("EEEEEchoooo!");
+    for (;;)
+    {
+        char ch = uart_get_char();
+        if (ch == -1)
+            continue;
+        uart_put_char(ch);
+    }
 }
