12b5c6ef97b186fe8117ff253289a85990831323
zzzly3 <1683994033@qq.com>
2021-09-29 22:36:23 +0800

    x

--- src/ob/mem.h
+++ src/ob/mem.h
@@ -2,7 +2,7 @@
 #define __MEM_H__
 
 #include <common/lutil.h>
-#include <common/mmu.h>
+#include <aarch64/mmu.h>
 #include <common/string.h>
 #include <core/physical_memory.h>
 #include <common/spinlock.h>
