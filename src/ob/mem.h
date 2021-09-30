09dcd36babfe7501f061a80a149e084918690c29
zzzly3 <1683994033@qq.com>
2021-09-30 23:16:40 +0800

    x

--- src/ob/mem.h
+++ src/ob/mem.h
@@ -15,6 +15,25 @@ typedef struct _MEMORY_SPACE
 	PPAGE_TABLE PageTable;
 } MEMORY_SPACE, *PMEMORY_SPACE;
 
+#define VPTE_USER PTE_USER
+#define VPTE_KERNEL PTE_KERNEL
+// Custom flags range from bit[12] to bit[15] + bit[1]
+#define VPTE_VALID 1 // Different from PTE_VALID
+#define VPTE_XN (1 << 12)
+#define VPTE_RES1 (1 << 13)
+#define VPTE_RES2 (1 << 14)
+#define VPTE_RES3 (1 << 15)
+typedef struct _VIRTUAL_PAGE
+{
+	ULONG Flags;
+	ULONG Id;
+} VIRTUAL_PAGE, *PVIRTUAL_PAGE;
+
+typedef struct _MEMORY_DESCRIPTOR
+{
+
+} MEMORY_DESCRIPTOR, *PMEMORY_DESCRIPTOR;
+
 void HalInitializeMemoryManager();
 BOOL MmInitializeMemorySpace(PMEMORY_SPACE);
 
