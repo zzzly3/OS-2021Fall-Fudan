e91506e242f40d752d01bce5ea4bac596190dcda
zzzly3 <1683994033@qq.com>
2021-09-30 23:37:43 +0800

    x

--- src/core/char_device.c
+++ src/core/char_device.c
@@ -9,3 +9,13 @@ void init_uart_char_device(CharDevice *device) {
     device->get = uart_get_char;
     device->put = uart_put_char;
 }
+
+void free_range(void *start, void *end) {
+    return;
+}
+void *kalloc(void) {
+    return NULL;
+}
+void kfree(void *page_address) {
+    return;
+}
