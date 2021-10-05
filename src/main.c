6b6f92fbc5970efa0a376ab4259ef97d2206eba9
zzzly3 <1683994033@qq.com>
2021-10-05 22:29:17 +0800

    x

--- src/main.c
+++ src/main.c
@@ -11,19 +11,15 @@ NORETURN void main() {
     
     if (!MmInitializeMemorySpace(&m))
         putchar('x');
-    puthex(MmGetPhysicalAddressEx(&m, 0xdeedbeef));
-    putchar(' ');
     char* p = (char*)MmAllocatePhysicalPage();
-    puthex((ULONG64)p);
-    putchar(' ');
-    if (!MmMapPageEx(&m, 0xdeedbeef, (ULONG64)p | PTE_USER_DATA))
+    if (!MmMapPageEx(&m, (PVOID)0xdeedbeef, (ULONG64)p | PTE_USER_DATA))
         putchar('x');
-    MmSwitchMemorySpace(NULL, &m);
+    MmSwitchMemorySpaceEx(NULL, &m);
     for (int i = 0; i < 4096; i++)
     {
         p[i] = "0123456789abcdef"[i & 15];
     }
     for (char* p = (char*)0xdeedbeef; p < (char*)0xdeedbeff; p++)
         putchar(*p);
-    
+
 }
