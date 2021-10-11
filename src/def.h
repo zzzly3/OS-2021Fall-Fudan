ed8ad4bed895e6d4ecc9a0632fea83f724f41e12
zzzly3 <1683994033@qq.com>
2021-10-11 23:32:11 +0800

    x

--- src/def.h
+++ src/def.h
@@ -6,8 +6,6 @@
 #include <driver/console.h>
 #include <ob/mem.h>
 
-#define putchar HalWriteConsoleChar
-
 static inline void system_init()
 {
 	HalInitializeDeviceManager();
@@ -15,74 +13,12 @@ static inline void system_init()
 	HalInitializeConsole();
 }
 
-static inline void puts(const char* s)
-{
-	KSTRING ks;
-	if (LibInitializeKString(&ks, s, 256))
-	{
-		HalWriteConsoleString(&ks);
-		HalWriteConsoleChar('\n');
-	}
-}
-
-static inline void putstr(const char* s)
-{
-	KSTRING ks;
-	if (LibInitializeKString(&ks, s, 256))
-		HalWriteConsoleString(&ks);
-}
-
-static inline void putdec(const long long n)
-{
-	char s[24];
-	KSTRING ks;
-	itos(n, s, 10);
-	LibInitializeKString(&ks, s, 23);
-	HalWriteConsoleString(&ks);
-}
-
-static inline void puthex(const long long n)
-{
-	char s[24];
-	KSTRING ks;
-	itos(n, s, 16);
-	LibInitializeKString(&ks, s, 23);
-	HalWriteConsoleString(&ks);
-}
-
-
-static inline char getchar()
-{
-	char c;
-	return KSUCCESS(HalReadConsoleChar(&c)) ? c : -1;
-}
-
-// Warning: This implementation is UNSAFE
-static inline int printf(const char *fmt, ...) {
-	char s[256];
-	int p = 0;
-    va_list arg;
-    va_start(arg, fmt);
-    while (*fmt && p < 239)
-    	if (*fmt == '%') switch (*++fmt)
-    	{
-    		#define __CASE(c, t, b) case c: p += itos(va_arg(arg, t), &s[p], b); fmt++; break;
-			__CASE('u', unsigned, 10)
-			__CASE('d', int, 10)
-			__CASE('x', unsigned, 16)
-			__CASE('p', ULONG64, 16)
-			__CASE('l', long long, 10)
-			#undef __CASE
-			case 's': for (CPCHAR q = va_arg(arg, CPCHAR); *q && p < 255;) s[p++] = *q++; fmt++; break;
-			case '\0': break;
-			default: goto put_char;
-		}
-    	else put_char:
-    		s[p++] = *fmt++;
-    va_end(arg);
-    s[p] = 0;
-    putstr(s);
-    return p;
-}
+#define putchar HalWriteConsoleChar
+void puts(const char*);
+void putstr(const char*);
+void putdec(const long long);
+void puthex(const long long);
+char getchar();
+int printf(const char*, ...);
 
 #endif
---
+++ src/lagacy.c
@@ -0,0 +1,72 @@
+#include <def.h>
+
+void puts(const char* s)
+{
+	KSTRING ks;
+	if (LibInitializeKString(&ks, s, 256))
+	{
+		HalWriteConsoleString(&ks);
+		HalWriteConsoleChar('\n');
+	}
+}
+
+void putstr(const char* s)
+{
+	KSTRING ks;
+	if (LibInitializeKString(&ks, s, 256))
+		HalWriteConsoleString(&ks);
+}
+
+void putdec(const long long n)
+{
+	char s[24];
+	KSTRING ks;
+	itos(n, s, 10);
+	LibInitializeKString(&ks, s, 23);
+	HalWriteConsoleString(&ks);
+}
+
+void puthex(const long long n)
+{
+	char s[24];
+	KSTRING ks;
+	itos(n, s, 16);
+	LibInitializeKString(&ks, s, 23);
+	HalWriteConsoleString(&ks);
+}
+
+char getchar()
+{
+	char c;
+	return KSUCCESS(HalReadConsoleChar(&c)) ? c : -1;
+}
+
+// Warning: This implementation is UNSAFE
+int printf(const char *fmt, ...)
+{
+	char s[256];
+	int p = 0;
+    va_list arg;
+    va_start(arg, fmt);
+    while (*fmt && p < 239)
+    	if (*fmt == '%') switch (*++fmt)
+    	{
+    		#define __CASE(c, t, b) case c: p += itos(va_arg(arg, t), &s[p], b); fmt++; break;
+			__CASE('u', unsigned, 10)
+			__CASE('d', int, 10)
+			__CASE('x', unsigned, 16)
+			__CASE('p', ULONG64, 16)
+			__CASE('l', long long, 10)
+			#undef __CASE
+			case 'c': s[p++] = va_arg(arg, char); fmt++; break;
+			case 's': for (CPCHAR q = va_arg(arg, CPCHAR); *q && p < 255;) s[p++] = *q++; fmt++; break;
+			case '\0': break;
+			default: goto put_char;
+		}
+    	else put_char:
+    		s[p++] = *fmt++;
+    va_end(arg);
+    s[p] = 0;
+    putstr(s);
+    return p;
+}
