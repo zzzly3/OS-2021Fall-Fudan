161b5af509c835fd280078fce9914da2230486c8
zzzly3 <1683994033@qq.com>
2021-10-16 00:25:13 +0800

    x

--- src/mod/scheduler.h
+++ src/mod/scheduler.h
@@ -32,6 +32,6 @@ RT_ONLY void KeTaskSwitch();
 APC_ONLY void KeClearApcList();
 RT_ONLY void KeClearDpcList();
 PDPC_ENTRY KeCreateDpc(PDPC_ROUTINE, ULONG64);
-PAPC_ENTRY KeCreateApcEx(PKPROCESS, PAPC_ROUTINE, ULONG64);
+PAPC_ENTRY KeCreateApcEx(struct _KPROCESS*, PAPC_ROUTINE, ULONG64);
 
 #endif
