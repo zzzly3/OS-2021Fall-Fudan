cdf138b14fccf4b1bc976ae6f115600b8d012fd5
zzzly3 <1683994033@qq.com>
2021-10-16 14:53:32 +0800

    x

---
+++ src/mod/syscall.c
@@ -0,0 +1,16 @@
+#include <mod/syscall.h>
+
+void KiSystemCallEntry(PTRAP_FRAME TrapFrame)
+{
+	ULONG64 callno = TrapFrame->x8;
+	ULONG64 arg1 = TrapFrame->x0;
+	ULONG64 arg2 = TrapFrame->x1;
+	switch (callno)
+	{
+		case SYS_myexecve: sys_myexecve(arg1); break;
+		case SYS_myexit: sys_myexit(); break;
+	}
+	// Fault if EL mismatching
+	KeRaiseExecuteLevel(EXECUTE_LEVEL_APC);
+	KeLowerExecuteLevel(EXECUTE_LEVEL_USR);
+}
