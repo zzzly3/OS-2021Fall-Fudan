65f32635feed7131f3e6323774ec9d1e038e3e90
zzzly3 <1683994033@qq.com>
2021-10-21 22:16:24 +0800

    x

--- src/common/lutil.h
+++ src/common/lutil.h
@@ -68,11 +68,11 @@ typedef struct _LIST_ENTRY
 	struct _LIST_ENTRY* Backward;
 } LIST_ENTRY, *PLIST_ENTRY;
 
-#define LibInsertListEntry(last_entry, new_entry) \
-	((new_entry)->Backward = (last_entry)->Backward)->Forward = (new_entry), \
-	((new_entry)->Forward = (last_entry))->Backward = (new_entry)
-#define LibRemoveListEntry(entry) \
-	((entry)->Forward->Backward = (entry)->Backward)->Forward = (entry)->Forward
+#define LibInsertListEntry(last_entry, new_entry) { PLIST_ENTRY __last = last_entry; \
+	((new_entry)->Backward = __last->Backward)->Forward = (new_entry); \
+	((new_entry)->Forward = __last)->Backward = (new_entry) }
+#define LibRemoveListEntry(entry) { PLIST_ENTRY __entry = entry; \
+	(__entry->Forward->Backward = __entry->Backward)->Forward = __entry->Forward; }
 #define LibPopSingleListEntry(head) ({typeof(head) __r = (head); \
 	if ((head) != NULL) (head) = (head)->NextEntry; \
 	__r;})
