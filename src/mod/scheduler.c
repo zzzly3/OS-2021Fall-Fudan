4135cb1827f37d2c8973a1f38246d469d7e71001
zzzly3 <1683994033@qq.com>
2021-10-21 22:30:26 +0800

    x

--- src/mod/scheduler.c
+++ src/mod/scheduler.c
@@ -251,6 +251,8 @@ RT_ONLY void PsiCheckInactiveList()
 			proc->Status = PROCESS_STATUS_RUNABLE;
 			LibRemoveListEntry(&proc->SchedulerList);
 			LibInsertListEntry(&KernelProcess->SchedulerList, &proc->SchedulerList);
+			uart_put_char('+');
+			uart_put_char('0' + proc->ProcessId);
 		}
 		p = np;
 	}
