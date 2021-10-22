26450e67fb2536714f3c817cd534d13ca8eecea5
zzzly3 <1683994033@qq.com>
2021-10-22 10:26:03 +0800

    x

--- src/mod/bug.c
+++ src/mod/bug.c
@@ -5,7 +5,7 @@ void KeBugFaultEx(CPCHAR BugFile, ULONG64 BugLine, ULONG64 BugId)
 {
 	arch_disable_trap();
 	delay_us(1000);
-	puts("\033[41;30m======KERNEL FAULT======\033[0m\n");
-	printf("Bug 0x%p detected in FILE %s, LINE %d\n", BugId, BugFile, BugLine);
+	puts("\033[41;33m======KERNEL FAULT======\033[0m");
+	printf("Bug \033[41;33m0x%p\033[0m detected in FILE %s, LINE %d\n", BugId, BugFile, BugLine);
 	while (1);
 }
