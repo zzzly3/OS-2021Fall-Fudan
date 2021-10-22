639f9ab0d53a966307d3cd23d7429e2cb39d8b3d
zzzly3 <1683994033@qq.com>
2021-10-22 23:40:49 +0800

    x

---
+++ src/mod/worker.c
@@ -0,0 +1,28 @@
+#include <mod/worker.h>
+
+// extern PKPROCESS KernelProcess[CPU_NUM]; // use self instead
+RT_ONLY void PsiCheckInactiveList();
+
+void KeSystemWorkerEntry()
+{
+	PKPROCESS cur = PsGetCurrentProcess();
+	while (1)
+	{
+		// Awake waiting process
+		KeRaiseExecuteLevel(EXECUTE_LEVEL_RT);
+		PsiCheckInactiveList();
+		KeLowerExecuteLevel(EXECUTE_LEVEL_USR);
+		if (cur->SchedulerList.Backward == &cur->SchedulerList)
+		{
+			// Scheduler list is empty
+			delay_us(TIME_SLICE_MS / 2);
+		}
+		else
+		{
+			// Give up the time
+			arch_disable_trap();
+			KeTaskSwitch();
+			arch_enable_trap();
+		}
+	}
+}
---
+++ src/mod/worker.h
@@ -0,0 +1,10 @@
+#ifndef __WORKER_H__
+#define __WORKER_H__
+
+#include <common/lutil.h>
+#include <ob/proc.h>
+#include <mod/scheduler.h>
+
+void KeSystemWorkerEntry();
+
+#endif
