fdbc84829a413c6474bb9695ad5f9269c8f8591d
zzzly3 <1683994033@qq.com>
2021-10-29 10:58:34 +0800

    x

--- src/core/test.c
+++ src/core/test.c
@@ -106,7 +106,7 @@ void sys_switch_test_proc(ULONG64 arg)
             if (!KSUCCESS(KeWaitForMutexSignaled(&mut2, TRUE)))
                 KeBugFault(BUG_STOP);
             KeLowerExecuteLevel(EXECUTE_LEVEL_USR);
-            printf("#2 proc %d run in cpu %d\n", n, cpuid());
+            //printf("#2 proc %d run in cpu %d\n", n, cpuid());
             for (int i = 0; i < 40; i++)
             {
                 for (int j = 0; j < a[n * 40 + i]; j++)
@@ -124,7 +124,7 @@ void sys_switch_test_proc(ULONG64 arg)
                 KeBugFault(BUG_CHECKFAIL);
             if (cnt == 200)
                 KeCreateDpc(sys_switch_test_proc, 3);
-            printf("#3 proc %d run in cpu %d\n", n, cpuid());
+            //printf("#3 proc %d run in cpu %d\n", n, cpuid());
             core2[cpuid()]++;
             KeSetMutexSignaled(&mut);
         } break;
