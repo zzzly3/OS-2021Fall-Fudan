b5483a069464928a5fbe14174bef47657feaf8a3
zzzly3 <1683994033@qq.com>
2021-11-08 00:24:10 +0800

    x

--- src/common/types.h
+++ src/common/types.h
@@ -3,13 +3,15 @@
 #ifndef _COMMON_TYPES_H_
 #define _COMMON_TYPES_H_
 
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+
 #ifndef __cplusplus
 #ifdef _WIN64
 typedef unsigned char bool;
 #else
 typedef _Bool bool;
 #endif
-
 
 #define true 1
 #define false 0
@@ -20,11 +22,11 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
 
 typedef int64_t ssize_t;
 typedef uint64_t size_t;
+#else
+#define restrict
 #endif
 
 /* Efficient min and max operations */
