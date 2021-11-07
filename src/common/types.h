7d606c2952c6bfc2463afc9912d499936d74d34d
zzzly3 <1683994033@qq.com>
2021-11-08 00:24:44 +0800

    x

--- src/common/types.h
+++ src/common/types.h
@@ -3,9 +3,6 @@
 #ifndef _COMMON_TYPES_H_
 #define _COMMON_TYPES_H_
 
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
-
 #ifndef __cplusplus
 #ifdef _WIN64
 typedef unsigned char bool;
@@ -22,6 +19,8 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
 
 typedef int64_t ssize_t;
 typedef uint64_t size_t;
