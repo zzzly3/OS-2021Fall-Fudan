c97991fcaae1c67d4551d21390fb5b288ffa3919
zzzly3 <1683994033@qq.com>
2021-11-09 17:30:58 +0800

    x

--- src/fs/inode.c
+++ src/fs/inode.c
@@ -329,6 +329,7 @@ static usize inode_insert(OpContext *ctx, Inode *inode, const char *name, usize 
     de.inode_no = inode_no;
     strncpy(de.name, name, FILE_NAME_MAX_LENGTH);
     usize i;
+    printf("i %s = %d\n", name, inode_no);
     inode_lookup(inode, NULL, &i);
     printf("i to %d\n", i);
     inode_write(ctx, inode, (u8*)&de, i, sizeof(DirEntry));
