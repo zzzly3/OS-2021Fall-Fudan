beb75fa907c97446d7d44041758808cd87095acd
zzzly3 <1683994033@qq.com>
2021-11-22 00:36:26 +0800

    x

--- src/core/sysproc_my.c
+++ src/core/sysproc_my.c
@@ -17,12 +17,12 @@ NO_RETURN void sys_myexit() {
             printf("pid %d, pid in root %d, cnt %d\n", getpid(), getrootpid(), x);
             yield(); */
 void sys_myprint(int x) {
-    int rootpid = -1;
-    for (int i = 0; i < NPID; i++) {
-        if (root_container->pmap[i].valid && root_container->pmap[i].p == thiscpu()->proc)
-            rootpid = root_container->pmap[i].pid_local;
-    }
-    assert(rootpid > 0);
-    printf("pid %d, pid in root %d, cnt %d\n", thiscpu()->proc->pid, rootpid, x);
-    yield();
+    // int rootpid = -1;
+    // for (int i = 0; i < NPID; i++) {
+    //     if (root_container->pmap[i].valid && root_container->pmap[i].p == thiscpu()->proc)
+    //         rootpid = root_container->pmap[i].pid_local;
+    // }
+    // assert(rootpid > 0);
+    // printf("pid %d, pid in root %d, cnt %d\n", thiscpu()->proc->pid, rootpid, x);
+    // yield();
 }
