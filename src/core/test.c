f523440a76523aac32b511be542e76dcdacef401
zzzly3 <1683994033@qq.com>
2021-11-22 01:04:00 +0800

    x

--- src/core/test.c
+++ src/core/test.c
@@ -244,6 +244,7 @@ void sys_group_test()
         p[i]->Flags |= PROCESS_FLAG_KERNEL;
         p[i]->Group = g;
         PsCreateProcess(p[i], sys_group_test_proc, i);
+        printf("?%d,r%d\n", i, p[i]->ReferenceCount.count);
         ObDereferenceObject(p[i]);
     }
     ObDereferenceObject(g);
