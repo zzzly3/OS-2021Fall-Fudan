eed7e4a2546105cb1275586f304fdfc67b0c935c
zzzly3 <1683994033@qq.com>
2021-12-08 01:00:40 +0800

    x

--- src/driver/sd.c
+++ src/driver/sd.c
@@ -645,6 +645,9 @@ void sd_intr() {
     int ival = (int)*EMMC_INTERRUPT;
     if (ival & INT_ERROR_MASK)
         PANIC("SD card error! EMMC_INTERRUPT=0x%x", ival);
+    printf("%x\n", ival);
+    *EMMC_INTERRUPT = ival;
+    return;
     PIOREQ_OBJECT req = (PIOREQ_OBJECT)SDDevice.DeviceStorage;
     if (req != NULL)
     {
