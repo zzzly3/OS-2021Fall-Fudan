22175dc883d08ef4676db92976523ef6d2d06d29
zzzly3 <1683994033@qq.com>
2021-12-08 00:41:06 +0800

    x

--- src/core/system.c
+++ src/core/system.c
@@ -11,6 +11,7 @@ void create_system_process()
 	static KSTRING system;
 	static int pid;
 	LibInitializeKString(&system, "system", 16);
-	KeCreateProcess(NULL, KeSystemEntry, NULL, &pid);
+	printf("%d %s\n", system.len, system.str);
+	KeCreateProcess(&system, KeSystemEntry, NULL, &pid);
 	printf("system is %d\n", pid);
 }
