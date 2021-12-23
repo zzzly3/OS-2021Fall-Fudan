d8b65e67e7791670966b52da1dc36319c2768c8e
zzzly3 <1683994033@qq.com>
2021-12-24 03:02:25 +0800

    x

--- src/fs/cache.c
+++ src/fs/cache.c
@@ -193,11 +193,11 @@ USR_ONLY
 static void cache_begin_op(OpContext *ctx) {
     // TODO
     static int funny = 0;
-    #ifdef UPDATE_API
-        KeUserWaitForMutexSignaled(&atomic_lock, FALSE);
-    #else
-        acquire_sleeplock(&atomic_lock);
-    #endif
+    // #ifdef UPDATE_API
+    //     KeUserWaitForMutexSignaled(&atomic_lock, FALSE);
+    // #else
+    //     acquire_sleeplock(&atomic_lock);
+    // #endif
     ctx->ts = ++funny;
 }
 
@@ -238,11 +238,11 @@ static void cache_end_op(OpContext *ctx) {
         cache_release(b);
     }
     header.num_blocks = 0;
-    #ifdef UPDATE_API
-        KeSetMutexSignaled(&atomic_lock);
-    #else
-        release_sleeplock(&atomic_lock);
-    #endif
+    // #ifdef UPDATE_API
+    //     KeSetMutexSignaled(&atomic_lock);
+    // #else
+    //     release_sleeplock(&atomic_lock);
+    // #endif
 }
 
 // see `cache.h`.
