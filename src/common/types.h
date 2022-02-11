b769673cb7aeb9a0cdad4ed17c4467c63fde7ae4
zzzly3 <1683994033@qq.com>
2022-02-11 22:47:03 +0800

    x

--- src/common/types.h
+++ src/common/types.h
@@ -19,11 +19,11 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
+// typedef signed long long int64_t;
+// typedef unsigned long long uint64_t;
 
-typedef int64_t ssize_t;
-typedef uint64_t size_t;
+// typedef int64_t ssize_t;
+// typedef uint64_t size_t;
 #else
 #define restrict
 #endif
