8403e551c5f99ca0f3adb21931dca449ab264b46
zzzly3 <1683994033@qq.com>
2022-02-14 21:20:17 +0800

    x

--- src/fs/fs.c
+++ src/fs/fs.c
@@ -3,11 +3,17 @@
 #include <fs/defines.h>
 #include <fs/fs.h>
 #include <fs/inode.h>
+#include <def.h>
 
 void init_filesystem() {
+    putstr("init_block_device\n");
     init_block_device();
 
     const SuperBlock *sblock = get_super_block();
+    printf("SuperBlock: log_start=%d inode_start=%d bitmap_start=%d num_blocks=%d num_inodes=%d\n",
+        sblock->log_start, sblock->inode_start, sblock->bitmap_start, sblock->num_blocks, sblock->num_inodes);
+    putstr("init_bcache\n");
     init_bcache(sblock, &block_device);
+    putstr("init_inodes\n");
     init_inodes(sblock, &bcache);
 }
