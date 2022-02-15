1d996965ef5333dd9d01981f86b4ddd609a650c9
zzzly3 <1683994033@qq.com>
2022-02-15 14:09:31 +0800

    x

--- src/user/mkdir/main.c
+++ src/user/mkdir/main.c
@@ -1,5 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void mymkdir(char *path) {
+    if (mkdir(path, 0) < 0)
+        printf("Unable to mkdir %s\n", path);
+}
 
 int main(int argc, char *argv[]) {
     /* TODO: Lab9 Shell */
-
+    for (int i = 1; i < argc; i++)
+        mymkdir(argv[i]);
+    return 0;
 }
